4828bac302c729e93040bf70a40dd99fceffb54e
Shep Bryan IV <shepperoni.pizza@gmail.com>
2023-12-01 15:11:13 -0500

    Tweaked model

--- model.py
+++ model.py
@@ -8,33 +8,35 @@ from torchvision import transforms
 class Generator(nn.Module):
     """Generator network."""
 
-    def __init__(self, img_shape=(64, 64), num_channels=1, conv_features=2, num_layers=3, latent_features=32):
+    def __init__(self, img_shape=(64, 64), num_channels=1, num_features=8, num_layers=4, num_steps=8, max_kT=1.5):
         super().__init__()
 
         # Set the parameters
         self.img_shape = img_shape
         self.num_channels = num_channels
+        self.num_features = num_features
         self.num_layers = num_layers
-        self.num_features = conv_features
-        self.latent_features = latent_features
-
-        # Calculate variables
-        pre_latent_shape = (img_shape[0] // 2**num_layers, img_shape[1] // 2**num_layers)
-        pre_latent_features = conv_features * 2**num_layers
-        pre_latent_features_total = pre_latent_features * pre_latent_shape[0] * pre_latent_shape[1]
+        self.num_steps = num_steps
+        self.max_kT = max_kT
+        
+        # Set temperature schedule
+        self.kT_schedule = list(torch.linspace(0, max_kT, num_steps))
 
         # Input block
         self.input = nn.Sequential(
-            nn.Conv2d(num_channels, conv_features, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(conv_features),
+            nn.Conv2d(num_channels, num_features, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features),
+            nn.ReLU(),
+            nn.Conv2d(num_features, num_features, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features),
             nn.ReLU(),
         )
 
         # Encoder block
         self.encoder = nn.ModuleList()
         for i in range(num_layers):
-            num_in = conv_features * 2**i
-            num_out = conv_features * 2**(i+1)
+            num_in = num_features * 2**i
+            num_out = num_features * 2**(i+1)
             self.encoder.append(nn.Sequential(
                 nn.Conv2d(num_in, num_out, kernel_size=3, stride=2, padding=1),
                 nn.BatchNorm2d(num_out),
@@ -43,24 +45,12 @@ class Generator(nn.Module):
                 nn.BatchNorm2d(num_out),
                 nn.ReLU(),
             ))
-        self.encoder.append(nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(pre_latent_features_total, latent_features),
-            nn.BatchNorm1d(latent_features),
-            nn.ReLU(),
-        ))
 
         # Decoder block
         self.decoder = nn.ModuleList()
-        self.decoder.append(nn.Sequential(
-            nn.Linear(latent_features, pre_latent_features_total),
-            nn.BatchNorm1d(pre_latent_features_total),
-            nn.ReLU(),
-            nn.Unflatten(-1, (pre_latent_features, *pre_latent_shape)),
-        ))
         for i in range(num_layers):
-            num_in = conv_features * 2**(num_layers-i)
-            num_out = conv_features * 2**(num_layers-i-1)
+            num_in = num_features * 2**(num_layers-i)
+            num_out = num_features * 2**(num_layers-i-1)
             self.decoder.append(nn.Sequential(
                 nn.ConvTranspose2d(num_in, num_out, kernel_size=3, stride=2, padding=1, output_padding=1),
                 nn.BatchNorm2d(num_out),
@@ -72,7 +62,10 @@ class Generator(nn.Module):
 
         # Output block
         self.output = nn.Sequential(
-            nn.Conv2d(conv_features, num_channels, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(num_features, num_features, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features),
+            nn.ReLU(),
+            nn.Conv2d(num_features, num_channels, kernel_size=3, stride=1, padding=1),
             nn.Sigmoid(),
         )
 
@@ -80,8 +73,11 @@ class Generator(nn.Module):
         """Number of parameters."""
         return sum(p.numel() for p in self.parameters())
 
-    def forward(self, x, kT=None):
+    def forward(self, x):
         """Forward pass."""
+
+        # Initialze skips
+        skips = []
 
         # Input
         x = self.input(x)
@@ -89,13 +85,11 @@ class Generator(nn.Module):
         # Encoder
         for i in range(len(self.encoder)):
             x = self.encoder[i](x)
-
-        # Thermal kick
-        if kT is not None:
-            x = x + kT * torch.randn_like(x)
-
+            skips.append(x)
+            
         # Decoder
         for i in range(len(self.decoder)):
+            x = x + skips[-i-1]
             x = self.decoder[i](x)
 
         # Output
@@ -103,15 +97,22 @@ class Generator(nn.Module):
 
         return x
     
-    def dream(self, x, kT=None, sigma=0.1, num_steps=100):
+    def dream(self, x, kT_schedule=None):
         """Dream."""
         
-        # Loop over the number of steps
-        for i in range(num_steps):
+        # Set the temperature schedule
+        if kT_schedule is None:
+            kT_schedule = self.kT_schedule
 
-            # Diffuse
-            x = x + sigma * torch.randn_like(x)
-            x = self.forward(x, kT=kT)
+        # Increase temperature
+        for kT in kT_schedule:
+            x = x + torch.randn_like(x, device=x.device) * kT
+            x = self(x)
+
+        # Decrease temperature
+        for kT in kT_schedule[-1::-1]:
+            x = x + torch.randn_like(x, device=x.device) * kT
+            x = self(x)
 
         # Return the result
         return x
--- training.py
+++ training.py
@@ -6,7 +6,6 @@ import torch
 import numpy as np
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
-from torchvision.transforms import GaussianBlur
 from model import Generator
 from torch.utils.data import DataLoader
 from data_generator import ChineseCharacters
@@ -20,12 +19,40 @@ image_size = (64, 64)
 lr = 0.001
 batch_size = 32
 num_batches = 100
-num_epochs = 1000
-num_steps = 5  # Number of times to add noise to the input image
+num_epochs = 20
 
-# Set variables
-# noise_levels = [2**(i+2-num_steps) for i in range(num_steps)]
-noise_levels = np.linspace(.01, 1.5, num_steps)
+
+
+### Set up Data ###
+
+# Set up the DataLoader for the dataset
+dataset = ChineseCharacters(image_size=image_size, transform=False)
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+
+### Set up Model ###
+
+# Define the generator model and the loss function
+generator = Generator()
+generator_path = "models/generator.pth"
+try:
+    # Try to load model weights
+    with open(generator_path, 'rb') as f:
+        generator.load_state_dict(torch.load(f))
+    print(f"Model found. Loading weights.")
+except:
+    # Initialize parameters to small random values
+    for param in generator.parameters():
+        param.data = torch.randn(param.shape) * .01
+    print(f"Model not found. Training from scratch.")
+generator.to(device)
+
+# Set up the optimizer
+optimizer = torch.optim.Adam(generator.parameters(), lr=lr)
+
+
+
+### Plotting Functions ###
 
 # Image viewer
 def view(image, id=0):
@@ -35,7 +62,7 @@ def view(image, id=0):
     return
 
 # Results viewer
-def view_results(inputs, outputs):
+def view_results(inputs, outputs, id=0):
     
     # Set up figure
     fig = plt.gcf()
@@ -56,9 +83,9 @@ def view_results(inputs, outputs):
     
     # Loop over images
     for i in range(num_images):
-        ax[0, i].set_title(f"Step {i+1}")
-        ax[0, i].imshow(inputs[i].detach().cpu().numpy()[0, 0, :, :], cmap='gray')
-        ax[1, i].imshow(outputs[i].detach().cpu().numpy()[0, 0, :, :], cmap='gray')
+        ax[0, i].imshow(inputs[i].detach().cpu().numpy()[id, 0, :, :], cmap='gray')
+        ax[1, i].imshow(outputs[i].detach().cpu().numpy()[id, 0, :, :], cmap='gray')
+        ax[1, i].set_xlabel(f"Step {i+1}", rotation=45)
 
     # Finalize figure
     plt.tight_layout()
@@ -68,39 +95,10 @@ def view_results(inputs, outputs):
     return fig, ax
 
 
-
-### Set up Data ###
-
-# Set up the DataLoader for the dataset
-dataset = ChineseCharacters(image_size=image_size)
-dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-
-
-### Set up Model ###
-
-# Define the generator model and the loss function
-generator = Generator()
-generator_path = "models/generator_auto1.pth"
-try:
-    # Try to load model weights
-    with open(generator_path, 'rb') as f:
-        generator.load_state_dict(torch.load(f))
-    print(f"Model found. Loading weights.")
-except:
-    # Initialize parameters to small random values
-    for param in generator.parameters():
-        param.data = torch.randn(param.shape) * .01
-    print(f"Model not found. Training from scratch.")
-generator.to(device)
-
-# Set up the optimizer
-optimizer = torch.optim.Adam(generator.parameters(), lr=lr)
-
-
 ### Train Model ###
 
 # Loop over epochs
-print("Training...")
+print(f"Training generator with {generator.numel()} parameters on {device}")
 for epoch in range(num_epochs):
     print(f"Epoch {epoch} of {num_epochs}")
 
@@ -126,50 +124,37 @@ for epoch in range(num_epochs):
             / sum(p.numel() for p in generator.parameters())
         )
 
-        # Train pure autoencoder
-        for i, sigma in enumerate(noise_levels):
-            
-            # Add noise
-            image = target_image.clone() + torch.randn_like(image, device=device) * sigma
-
-            # Forward pass
-            image = generator(image)
-
-            # Calculate loss
-            loss += F.mse_loss(image, target_image)
-
-        # Train pyramid model
+        # Increase temperature
         image = target_image.clone()
-        for i, sigma in enumerate(noise_levels):
-            last_loop = i == len(noise_levels) - 1
+        for i, kT in enumerate(generator.kT_schedule):
+
+            # Check if end of schedule
+            last_step = i == generator.num_steps - 1
             
             # Add noise
-            image = image + torch.randn_like(image, device=device) * sigma
+            image = image + torch.randn_like(image, device=device) * kT
             input_images.append(image.clone())
 
             # Forward pass
             image = generator(image)
             output_images.append(image.clone())
-
-            # Calculate loss
-            loss += F.mse_loss(image, target_image)
-
-            # Loop down the pyramid
-            image2 = image.clone()
-            for j, sigma2 in enumerate(noise_levels[:i][::-1]):
-
+            
+            # Loop down temperature schedule from i to 0
+            for j in range(i-1, -1, -1):
+                kT2 = generator.kT_schedule[j]
+                
                 # Add noise
-                image2 = image + torch.randn_like(image2, device=device) * sigma2
-                if last_loop:
-                    input_images.append(image2.clone())
-
+                image = image + torch.randn_like(image, device=device) * kT2
+                if last_step:
+                    input_images.append(image.clone())
+                
                 # Forward pass
-                image2 = generator(image2)
-                if last_loop:
-                    output_images.append(image2.clone())
+                image = generator(image)
+                if last_step:
+                    output_images.append(image.clone())
 
-                # Calculate loss
-                loss += F.mse_loss(image2, target_image)
+            # Add loss
+            loss += F.mse_loss(image, target_image)
 
         # Backpropagate
         loss.backward()
@@ -177,13 +162,19 @@ for epoch in range(num_epochs):
 
         # Print update
         if batch_idx % 10 == 0:
-            print(f"Batch {batch_idx} of {num_batches}: loss = {loss.item():.4f} ({(time.time()-t):.2f} s)")
+            status = (
+                f"Batch {batch_idx} of {num_batches}:"
+                + f" loss = {loss.item():.4f} ({(time.time()-t):.2f} s)"
+            )
+            print(status)
             view_results(input_images, output_images)
 
     # Dream
     generator.eval()
-    image = torch.zeros((1, 1, *image_size), device=device)
-    image = generator.dream(image, kT=0, sigma=1.0, num_steps=100)
+    image = generator.max_kT * torch.randn((1, 1, *image_size), device=device)
+    image = next(iter(dataloader)).to(device)[[0]]
+    for _ in range(10):
+        image = generator.dream(image)
     plt.clf()
     view(image)
     
